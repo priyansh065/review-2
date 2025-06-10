9d1b205b143c843302936e45da5a096906982bb8
shipra3s <shipra.24sece1030056@galgotiasuniversity.ac.in>
2025-06-10 23:18:20 +0530

    Add files via upload

---
+++ Frontpage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.frontpage;
+
+/**
+ *
+ * @author shals
+ */
+public class Frontpage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
